bf70fae3dd31acaa019adfb403181418003c9edb
Mahdi Kashkalani <mahdiksk88@gmail.com>
2025-01-31 17:48:22 +0330

    Change the default class name and make it for DI service registrations

---
+++ src/Core/Shopping.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Shopping.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection RegisterApplicationServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
